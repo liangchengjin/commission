3df3785a70e507d0b50c5cd3d49d9b03e6b477f3
chengjin <chengjin@sina.com>
2016-10-10 18:07:36 +0800

    all

--- script/common/utls.js
+++ script/common/utls.js
@@ -1,15 +1,3 @@
-// jing
-var line = $('.line');
-$('.inform-list li').on('tap',function(){
-	if($(this).index() == 0 ){
-		line.css('left','0');
-	} else if($(this).index() == 1){
-		line.css('left','100%');
-	} else {
-		line.css('left','200%');
-	}
-})
-
 
 /****************************************
  *			   按钮颜色     			    *
@@ -24,12 +12,8 @@ $('.inform-list li').on('tap',function(){
 	};
 
     $('.inform-tab input').on('tap',function(){
-
 		 butColor(this)
 	});
-
-
-
 /****************************************
  *			 底部按钮 颜色     			*
  ****************************************/
@@ -41,9 +25,7 @@ $('.inform-list li').on('tap',function(){
 	$("body").on("touchend","#footer-btn", function(){
 			this.style.background = objColor.oranga;
 			this.style.color = objColor.fontWhite;
-	   });
-
-
+	  });
    function butColor(selector){
 	if($(selector).attr("checked") === false){
 		$('.footer-btn').css('background','#cccccc');
@@ -80,7 +62,6 @@ $('.inform-list li').on('tap',function(){
 		this.style.color = objColor.fontWhite;
 
 	});
-
 /****************************************
  *			收货地址 单选切换 			*
  ****************************************/
@@ -93,16 +74,14 @@ $('.inform-list li').on('tap',function(){
 		.siblings("tr").find(".iconchoicedefault").removeClass("iconfont icon-iconchoicedefault");
 
 		$(this).parents("tr").find(".inform-selector").css("border","0px").parents("tr").siblings("tr").
-		find(".inform-selector").css("border","0.05rem solid #cccccc;");
+		find(".inform-selector").css("border","0.02rem solid #cccccc;");
 
 		$(this).parents("tr").find(".inform-td-icon").css("border","0px").parents("tr").siblings("tr").
-		find(".inform-td-icon").css("border","0.05rem solid #cccccc;");
+		find(".inform-td-icon").css("border","0.02rem solid #cccccc;");
 
 		$(".footer .footer-activeaddress-btn").css("background","rgb(255, 144, 0)");;
 
 	});
-
-
 /****************************************
 *			保存手机号码					*
 ****************************************/
@@ -112,17 +91,22 @@ $('.inform-list li').on('tap',function(){
 		}
 	});
 
-	$(".inform-input .user-tel").keyup(function(){
-		if(!$(".inform-input .user-tel").val()){
-			$(".button-box .button-end").css("background","rgb(204, 204, 204);");
-			return;
-		}
-//		console.log(!$(".inform-input .user-name").val());
-//		console.log(!$(".inform-input .user-tel").val());
-		if($(".inform-input .user-name").val() || $(".inform-input .user-tel").val()){
-//			console.log("99999");
+	$(".inform-input .inform-user-name").keyup(function(){
+		if($(".inform-input .user-tel").val() !== '' && $(".inform-input .user-name").val() !== ''){
 			$(".button-box .button-end").css("background","rgb(255, 144, 0)");
+		} else{
+			$(".button-box .button-end").css("background","rgb(204, 204, 204);");
 		}
+//		if($(".inform-input .user-tel").val() && !$(".inform-input .user-name").val()){
+//			
+//		}
+//		if(!$(".inform-input .user-tel").val()){
+//			$(".button-box .button-end").css("background","rgb(204, 204, 204);");
+//			return;
+//		}
+//		if($(".inform-input .user-name").val() || $(".inform-input .user-tel").val()){
+//			$(".button-box .button-end").css("background","rgb(255, 144, 0)");
+//		}
 	});
 	
 	$(".button-box .button-end").tap(function(){
@@ -132,75 +116,21 @@ $('.inform-list li').on('tap',function(){
 			return
 		}
 		if(!$(".inform-input .user-tel").val()){
-			alert("请输入收货人联系电话");
+			show.popupTip("请输入收货人联系电话");
 		} else if(!(/^1[3|4|5|7|8]\d{9}$/.test($(".inform-input .user-tel").val()))){
 			show.popupTip('请输入正确的手机号码')
+			$(".inform-input .user-tel").val("");
 		}else{
 			$('#form_address').submit();
 		}
-
-		$(".inform-input .user-name").val("");
-		$(".inform-input .user-tel").val("");
+//		$(".inform-input .user-name").val("");
+//		$(".inform-input .user-tel").val("");
 		$(".button-box .button-end").css("background","rgb(204, 204, 204);");
 	});
 
 	
 
 var show = {
-// 弹出框
-	popupTip:function (tip,cancelText,sureText,cancelCallback,sureCallback) {
-		var tip= tip,
-			cancelText = cancelText,
-			sureText = sureText;
-
-			if ($("#dialog3").length === 0){
-				var $div = '<div class="ui-dialog" id="dialog3">' +
-					'<div class="ui-dialog-cnt"> ' +
-					'<div class="ui-dialog-bd">' +
-					'<h3></h3> ' +
-					'</div> ' +
-					'<div class="ui-dialog-ft"> ' +
-					'<a class="ui-btn" id = "showCancel"></a>' +
-					'<a class="ui-btn ui-btn-primary" id = "showSure"></a>' +
-					'</div>' +
-					'</div>' +
-					'</div>';
-				$('body').append($div);
-			}
-		$('.ui-dialog-bd h3').text(tip);
-		$('#showCancel').text(cancelText);
-		$('#showSure').text(sureText);
-
-		if (!cancelText && !sureText){
-			var $btn = '<a class="ui-btn ui-btn-primary tipSure" id = "tipSure">确定</a>';
-			$('#showCancel,#showSure').hide();
-			if ($(".tipSure").length == 1){
-				return false;
-			} else{
-				$('.ui-dialog-ft').append($btn);
-			}
-		} else {
-			$('#showCancel,#showSure').show();
-		}
-		$("#dialog3").show();
-		document.documentElement.style.overflow='hidden';
-		$('#showCancel').unbind().on('tap', function () {
-			$("#dialog3").hide();
-			document.documentElement.style.overflow='auto';
-			cancelCallback&&cancelCallback();
-		});
-		$('#showSure').unbind().on('tap', function () {
-			$("#dialog3").hide();
-			document.documentElement.style.overflow='auto';
-			sureCallback&&sureCallback();
-		});
-		$('#tipSure').unbind().on('tap', function () {
-			$("#dialog3").hide();
-			document.documentElement.style.overflow='auto';
-			$(this).remove();
-		});
-	},
-	
 //按钮颜色	
 	butColor:function(selector,event,bg,color){
 	    $(selector).on(event,function(){
@@ -210,51 +140,52 @@ var show = {
 	        })
 	    });
 	},
-	
-//跳转
-	jump:function(url,el){
-			$.ajax({
-			type:"POST",
-			url:url,
-            data:el,
-			dataType:"json",
-			beforeSend:function () {},
-			success:function(rs){
-				if(rs.error){
-					if (rs.message){
-						show.popupTip(rs.message);
-						return false;
-					} else{
-
-					}
-				} else{
-					if(rs.redirect){						
-						location.href = rs.redirect;
-					} if(rs.success){
-						
-					}
-				}
-			},
-			error:function(xhr,type){},
-			complete:function(){
-				$('.btnJump').removeAttr('disabled');
-			}
-
-		});
-	}
+//	
+////跳转
+//	jump:function(url,el){
+//			$.ajax({
+//			type:"POST",
+//			url:url,
+//          data:el,
+//			dataType:"json",
+//			beforeSend:function () {},
+//			success:function(rs){
+//				if(rs.error){
+//					if (rs.message){
+//						show.popupTip(rs.message);
+//						return false;
+//					} else{
+//
+//					}
+//				} else{
+//					if(rs.redirect){						
+//						location.href = rs.redirect;
+//					} if(rs.success){
+//						
+//					}
+//				}
+//			},
+//			error:function(xhr,type){},
+//			complete:function(){
+//				$('.btnJump').removeAttr('disabled');
+//			}
+//
+//		});
+//	}
 };
 
-$('.btnJump').unbind().on('tap',function(event){
-	$(this).attr('disabled','disabled');
-	var formUrl = $(this).parents("form").attr("action"),
-		btnUrl = $(this).attr('data-url'),
-		element;
-	if (!($(this).parents("form").length==0)){
-		if ($(this).parents("form").serialize() == ""){
-			element = "";
-		}else {
-			element = $(this).parents("form").serialize();
-		}
-	}
-	show.jump(formUrl || btnUrl,element);
-})
+//
+//$('.btnJump').unbind().on('tap',function(event){
+//	$(this).attr('disabled','disabled');
+//	var formUrl = $(this).parents("form").attr("action"),
+//		btnUrl = $(this).attr('data-url'),
+//		element;
+//	if (!($(this).parents("form").length==0)){
+//		if ($(this).parents("form").serialize() == ""){
+//			element = "";
+//		}else {
+//			element = $(this).parents("form").serialize();
+//		}
+//	}
+//	show.jump(formUrl || btnUrl,element);
+//})
---
+++ script/wxSingleProduct.js
@@ -0,0 +1,191 @@
+var container = $('.restrict').html()
+var startsTime = $('#starttimestamp').val();
+var endTimes = $('#endtimestamp').val();
+$('#number').val(container);
+var productDetail = {
+    init:function () {
+    	this.time(startsTime,endTimes);
+		setInterval(function(){
+			productDetail.time(startsTime,endTimes);	
+			}, 1000);
+//      this.mainHeight();
+        this.bindEvent();
+        this.swiper();
+        this.statusFn();
+        productDetail.butColor('.btnJump ','touchstart','#cc7300','#cccccc');
+		productDetail.butColor('.btnJump ','touchend','#ff9000','#ffffff');	
+    },
+    swiper: function () {
+        if ($(".swiper-wrapper .swiper-slide").length == 0){
+            var $img = '<div class="swiper-slide"><img src="images/no-product.png"></div>';
+            $(".swiper-wrapper").append($img);
+
+        } else if ($(".swiper-wrapper .swiper-slide").length == 1){
+
+        } else{
+            var swiper = new Swiper('.swiper-container',{
+                autoplay : 2000,
+                pagination : '.swiper-pagination',
+                autoplayDisableOnInteraction: false,
+                loop : true
+            }) ;
+        }
+    },
+    time:function(starts,ends){
+				ends   = ends   * 1;
+				starts = starts * 1
+				var ts;
+				var endTime = ends ;
+				var startTime = starts;
+				var d=0;
+				var h=0;
+				var m=0;
+				var s=0;
+				var nowTime = new Date();
+				var nowSec = Math.floor((nowTime.getTime())/1000);
+//			console.log(nowSec)
+				if(startTime >= nowSec){
+					$('.status').html('开始')
+					$('#bug').html('未开始')
+					$('#bug').addClass('grayBg');
+					t = startTime - nowSec;
+					d=Math.floor(t/60/60/24);
+					h=Math.floor(t/60/60%24);
+					m=Math.floor(t/60%60);
+					s=Math.floor(t%60);
+					$('.footer-btn').removeClass('btnJump');
+				}else{
+					$('.status').html('结束')
+					t = endTime - nowSec;
+					$('#bug').html('马上抢')
+					$('#bug').removeClass('grayBg');
+					productDetail.statusFn();
+					if(t>0){
+						d=Math.floor(t/60/60/24);
+						h=Math.floor(t/60/60%24);
+						m=Math.floor(t/60%60);
+						s=Math.floor(t%60);
+					}else{
+						$('#bug').html('已结束');						
+						$('#bug').addClass('grayBg');
+						$('.footer-btn').removeClass('btnJump');
+						$('.right').hide()
+					}
+				}				
+					(d < 10)? (d = '0' + d) : (d = d);
+					(h < 10)? (h = '0' + h) : (h = h);	
+					(m < 10)? (m = '0' + m) : (m = m);	
+					(s < 10)? (s = '0' + s) : (s = s);
+				$('.day').html(d)
+				$('.hour').html(h)	
+				$('.minute').html(m)	
+				$('.sec').html(s)					
+		},
+//  mainHeight:function () {
+//      var clientH =  $('body').height(),
+//          footerH = $('.footer').height(),
+//          navH = $(".navbar").height(),
+//          productH = clientH -footerH -navH;
+//      $('.product').css('height',productH + 'px');
+//  },
+    bindEvent:function () {
+        this.reduceNumber();
+        this.addNumber();
+        this.enterNumber();
+        $('#myTab>li').on('tap',function(){
+            if($(this).index() === 0 ){
+            	$('.line').css('left','0%')
+                $('#product-info').removeClass('tab-active');
+                $('#product-param').addClass('tab-active');
+
+            } else if($(this).index() === 1){
+            	$('.line').css('left','100%')
+                $('#product-info').addClass('tab-active');
+                $('#product-param').removeClass('tab-active');
+            }
+        });
+    },
+    enterNumber:function () {
+        $('#number').on('keyup',function () {
+            var number = $('#number').val();
+            if (number) {
+                if (number > 1 && number < container) {
+                    $('#reduce').addClass('number-active');
+                	$('#add').removeClass('number-active');
+                }else if (number < 1){
+                    $('#reduce').addClass('number-active');
+                    $('#number').val(1);
+                } else if (number >= container){
+                    $('#add').addClass('number-active');
+                	$('#number').val(container);
+                } else {
+                	$('#add').removeClass('number-active');
+                	$('#reduce').removeClass('number-active');
+                }
+            }
+        });
+    },
+	reduceNumber:function () {
+        $('#reduce').on("tap",function () {
+            var number = $('#number').val();    
+            number--;
+            $('#number').val(number);
+            if (number > 1) {
+                $('#reduce').removeClass('number-active');
+                $('#add').removeClass('number-active');
+            }else{
+                $('#reduce').addClass('number-active');        
+                $('#number').val(1);
+            }
+
+        });
+    },
+    addNumber:function () {
+        $('#add').on("tap",function () {
+            var number = $('#number').val();
+            number++;
+            $('#number').val(number);
+            if (number >= container) {
+                $('#add').addClass('number-active');
+                $('#number').val(container);
+            }else{
+                $('#add').removeClass('number-active');
+                $('#reduce').removeClass('number-active');               
+            }
+        });
+    },
+//按钮颜色	
+	butColor:function(selector,event,bg,color){
+	    $(selector).on(event,function(){
+	        $(this).css({
+	            'background':bg,
+	            'color':color
+	        })
+	    });
+	},
+	statusFn:function(){
+		switch ($('#bug').attr('stus')) {
+			case '3':
+			productDetail.statusContent("抢光了");					
+			break;
+			case '5':
+			productDetail.statusContent("已参与")		
+			break;
+			case '6':
+			productDetail.statusContent("已中止")			
+			break;
+			case '7':
+			productDetail.statusContent("已失效")
+			break;
+		}
+	},
+	statusContent:function(txt){
+		$('#bug').html(txt);
+		$('#bug').removeClass('btnJump');
+		$('#bug').addClass('grayBg');	
+	}
+}
+$(function () {
+    productDetail.init()
+});
+
